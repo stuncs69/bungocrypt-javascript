d532e38ca1c4eb28a39541068755d49e77d7980c
Graph <68465860+stuncs69@users.noreply.github.com>
2022-07-11 18:45:40 +0100

    Added switch cases instead of the long else if

--- index.js
+++ index.js
@@ -28,141 +28,98 @@ export function Encrypt(data, key) {
 
     for (var i = 0; i < data.length; i++) {
         dataChar = data[i].toLowerCase();
-
-        if (dataChar == "a") {
-            encryptedString += key[0]
+        switch (dataChar) {
+            case "a":
+                encryptedString += key[0];
+            case "b":
+                encryptedString += key[1];
+            case "c":
+                encryptedString += key[2];
+            case "d":
+                encryptedString += key[3];
+            case "e":
+                encryptedString += key[4];
+            case "f":
+                encryptedString += key[5];
+            case "g":
+                encryptedString += key[6];
+            case "h":
+                encryptedString += key[7];
+            case "i":
+                encryptedString += key[8];
+            case "j":
+                encryptedString += key[9];
+            case "k":
+                encryptedString += key[10];
+            case "l":
+                encryptedString += key[11];
+            case "m":
+                encryptedString += key[12];
+            case "n":
+                encryptedString += key[13];
+            case "o":
+                encryptedString += key[14];
+            case "p":
+                encryptedString += key[15];
+            case "q":
+                encryptedString += key[16];
+            case "r":
+                encryptedString += key[17];
+            case "s":
+                encryptedString += key[18];
+            case "t":
+                encryptedString += key[19];
+            case "u":
+                encryptedString += key[20];
+            case "v":
+                encryptedString += key[21];
+            case "w":
+                encryptedString += key[22];
+            case "x":
+                encryptedString += key[23];
+            case "y":
+                encryptedString += key[24];
+            case "z":
+                encryptedString += key[25];
+            case "1":
+                encryptedString += key[26];
+            case "2":
+                encryptedString += key[27];
+            case "3":
+                encryptedString += key[28];
+            case "4":
+                encryptedString += key[29];
+            case "5":
+                encryptedString += key[30];
+            case "6":
+                encryptedString += key[31];
+            case "7":
+                encryptedString += key[32];
+            case "8":
+                encryptedString += key[33];
+            case "9":
+                encryptedString += key[34];
+            case "0":
+                encryptedString += key[35];
+            case "!":
+                encryptedString += key[36];
+            case "@":
+                encryptedString += key[37];
+            case "#":
+                encryptedString += key[38];
+            case "$":
+                encryptedString += key[39];
+            case "(":
+                encryptedString += key[40];
+            case ")":
+                encryptedString += key[41];
+            case "*":
+                encryptedString += key[42];
+            case " ":
+                encryptedString += key[43];
         }
-        else if (dataChar == "b") {
-            encryptedString += key[1]
-        }
-        else if (dataChar == "c") {
-            encryptedString += key[2]
-        }
-        else if (dataChar == "d") {
-            encryptedString += key[3]
-        }
-        else if (dataChar == "e") {
-            encryptedString += key[4]
-        }
-        else if (dataChar == "f") {
-            encryptedString += key[5]
-        }
-        else if (dataChar == "g") {
-            encryptedString += key[6]
-        }
-        else if (dataChar == "h") {
-            encryptedString += key[7]
-        }
-        else if (dataChar == "i") {
-            encryptedString += key[8]
-        }
-        else if (dataChar == "j") {
-            encryptedString += key[9]
-        }
-        else if (dataChar == "k") {
-            encryptedString += key[10]
-        }
-        else if (dataChar == "l") {
-            encryptedString += key[11]
-        }
-        else if (dataChar == "m") {
-            encryptedString += key[12]
-        }
-        else if (dataChar == "n") {
-            encryptedString += key[13]
-        }
-        else if (dataChar == "o") {
-            encryptedString += key[14]
-        }
-        else if (dataChar == "p") {
-            encryptedString += key[15]
-        }
-        else if (dataChar == "q") {
-            encryptedString += key[16]
-        }
-        else if (dataChar == "r") {
-            encryptedString += key[17]
-        }
-        else if (dataChar == "s") {
-            encryptedString += key[18]
-        }
-        else if (dataChar == "t") {
-            encryptedString += key[19]
-        }
-        else if (dataChar == "u") {
-            encryptedString += key[20]
-        }
-        else if (dataChar == "v") {
-            encryptedString += key[21]
-        }
-        else if (dataChar == "w") {
-            encryptedString += key[22]
-        }
-        else if (dataChar == "x") {
-            encryptedString += key[23]
-        }
-        else if (dataChar == "y") {
-            encryptedString += key[24]
-        }
-        else if (dataChar == "z") {
-            encryptedString += key[25]
-        }
-        else if (dataChar == "1") {
-            encryptedString += key[26]
-        }
-        else if (dataChar == "2") {
-            encryptedString += key[27]
-        }
-        else if (dataChar == "3") {
-            encryptedString += key[28]
-        }
-        else if (dataChar == "4") {
-            encryptedString += key[29]
-        }
-        else if (dataChar == "5") {
-            encryptedString += key[30]
-        }
-        else if (dataChar == "6") {
-            encryptedString += key[31]
-        }
-        else if (dataChar == "7") {
-            encryptedString += key[32]
-        }
-        else if (dataChar == "8") {
-            encryptedString += key[33]
-        }
-        else if (dataChar == "9") {
-            encryptedString += key[34]
-        }
-        else if (dataChar == "0") {
-            encryptedString += key[35]
-        }
-        else if (dataChar == "!") {
-            encryptedString += key[36]
-        }
-        else if (dataChar == "@") {
-            encryptedString += key[37]
-        }
-        else if (dataChar == "#") {
-            encryptedString += key[38]
-        }
-        else if (dataChar == "$") {
-            encryptedString += key[39]
-        }
-        else if (dataChar == "(") {
-            encryptedString += key[40]
-        }
-        else if (dataChar == ")") {
-            encryptedString += key[41]
-        }
-        else if (dataChar == "*") {
-            encryptedString += key[42]
-        }
-        else if (dataChar == " ") {
-            encryptedString += key[43]
-        }
-    }
     return encryptedString;
+}
 }
 
 export function Decrypt(data, key) {
@@ -172,7 +129,7 @@ export function Decrypt(data, key) {
     else if (key.length != 44) {
         return console.error("KeyError[2]: Key is not 44 characters!");
     }
-    else if (!data) {
+    else if(!data) {
         return console.error("DataError[1]: Data is missing!");
     }
 
